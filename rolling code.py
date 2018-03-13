956f3380dd5d9508ab03653ce44602e143885c54
brent <brent.faust@am.sony.com>
2018-03-12 19:35:10 -0700

    error fixed

--- rolling code.py
+++ rolling code.py
@@ -2,7 +2,7 @@
 
 # from February 8 2017 to
 # Malibu, California
-#
+# San Diego, California
 
 # source patent US 6980655
 
@@ -16,6 +16,8 @@
 # making the rolling code
 #
 
+# Python 3.5 code
+
 
 from numpy import *
 import random
@@ -24,12 +26,19 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
+
 def run(total_bit):
+    each_digit = 0
+    rolling_code = []
     for i in range(total_bit):
-        float (each_digit = random.random())
-        rolling_code = chr(each_digit)
+        if random.random() > 0.5:
+            each_digit = random.randint(65, 91)
+        else:
+            each_digit = random.randint(97, 123)
+        rolling_code.append(chr(each_digit))
     return rolling_code
-    
+
+
 if __name__ == '__main__':
     total_bit = 40;
     rolling_code = run(total_bit)
